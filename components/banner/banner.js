30d8dc40cb1f4a12dbd6cdf0c5825da156062130
muni <booscarface@gmail.com>
2023-06-14 12:31:09 +0200

    title and subtitle

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -13,8 +13,13 @@ export default function Banner() {
                         <div className={styles.brandNameWrapper}>
                             <p className={styles.article}>The</p>
                             <p className={styles.brandName}> Ugly <br/> Cat</p>
-                            
                         </div>
+                        <h1 className={styles.title}>
+                            Sasha and Taylor Swift
+                        </h1>
+                        <h3 className={styles.subtitle}>
+                            Dictators on Directors
+                        </h3>
                     </div>
                 </div>
             </div>
