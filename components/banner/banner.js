8c6d9b68210b2e90ee24b32714695aa067d86950
muni <booscarface@gmail.com>
2023-06-14 11:56:16 +0200

    brand name - styling

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -1,4 +1,4 @@
-
+import cls from 'classnames'
 import styles from './banner.module.css'
 
 export default function Banner() {
@@ -7,6 +7,17 @@ export default function Banner() {
     }
     return (
         <div className={styles.container}>
+            <div className={styles.wrapper}>
+                <div className={styles.left}>
+                    <div className={cls("glass", styles.glassWrapper)}>
+                        <div className={styles.brandNameWrapper}>
+                            <p className={styles.article}>The</p>
+                            <p className={styles.brandName}> Ugly <br/> Cat</p>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div 
                 className={styles.bannerImg}
                 style={{
