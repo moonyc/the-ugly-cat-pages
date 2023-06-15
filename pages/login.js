99f82a4cd0106af2e0c645c1320eda72d5c2ffb5
muni <booscarface@gmail.com>
2023-06-15 17:12:58 +0200

    apparently nothing was up with magic

--- pages/login.js
+++ pages/login.js
@@ -19,7 +19,7 @@ export default function Login() {
     const handleLoginWithEmail = async (e) => {
         e.preventDefault()
         if(email) {
-            if( email ) {
+            if( email === "municfara@gmail.com") {
                 try {
                     const didToken = await magic.auth.loginWithEmailOTP({ email: email })
                     if(didToken) {
