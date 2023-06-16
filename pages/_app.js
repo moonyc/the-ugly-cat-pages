270aa4bb959535bb4a10b35b3984c4c323bb9f99
muni <booscarface@gmail.com>
2023-06-16 16:24:27 +0200

    loading...

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,11 @@
 import '@/styles/globals.css'
-import { useEffect } from 'react'
+import { useEffect, useState} from 'react'
 import { useRouter } from 'next/router'
 import { magic } from '@/lib/magic'
 
 export default function App({ Component, pageProps }) {
   const router = useRouter()
+  const [isLoading, setIsLoading]= useState(true)
 
   useEffect(() => {
     const handleLogin = async () => {
@@ -17,6 +18,19 @@ export default function App({ Component, pageProps }) {
     }
     handleLogin()
   }, [])
-  
-  return <Component {...pageProps} />
+
+  useEffect(() => {
+    const handleComplete = () => {
+      setIsLoading(false)
+    }
+    router.events.on('routeChangeComplete', handleComplete)
+    router.events.on('routeChangeError', handleComplete)
+
+    return () => {
+      router.events.off("routeChangeComplete", handleComplete)
+      router.events.off("routeChangeError", handleComplete)
+    }
+  },[router])
+
+  return isLoading ? <div>Loading...</div> : <Component {...pageProps} />
 }
