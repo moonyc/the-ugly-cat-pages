6f9f76dc46a224462133de34aeebd4513b871f36
muni <booscarface@gmail.com>
2023-06-16 15:30:53 +0200

    handling routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { magic } from '@/lib/magic'
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleLogin = async () => {
+      const isLoggedIn = await magic.user.isLoggedIn()
+      if(isLoggedIn) {
+        router.push('/')
+      } else {
+        router.push('/login')
+      }
+    }
+    handleLogin()
+  }, [])
   return <Component {...pageProps} />
 }
