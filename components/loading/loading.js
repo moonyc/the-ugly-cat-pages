fb7ec70a0dffe406ad714c901bb5bd8f0a6ef26e
muni <booscarface@gmail.com>
2023-06-16 17:31:31 +0200

    loading-final?

--- components/loading/loading.js
+++ components/loading/loading.js
@@ -1,8 +1,20 @@
 import styles from './loading.module.css'
+import Image from 'next/image'
 
 export default function Loading() {
     return (
         <main className={styles.mainLoading}>
+            <div className={styles.logoWrapper}>
+                    <Image
+                        src="/the-ugly-cat.svg"
+                        width={50}
+                        height={50}
+                        alt="the ugly cat logo"
+                    />
+                    <p className={styles.theUglyCat}>
+                                The Ugly Cat
+                    </p>
+             </div>
             <div className={styles.container}>
                 <div className={styles.Loading}></div>
             </div>
