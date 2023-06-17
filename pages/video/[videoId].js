21b33c74874ce2a446d7562adfd7e767cc146493
muni <booscarface@gmail.com>
2023-06-17 10:28:41 +0200

    webp

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -2,6 +2,7 @@ import { useRouter } from "next/router"
 import Modal from 'react-modal'
 import styles from '../../styles/Video.module.css'
 import clsx from 'classnames'
+import Head from "next/head"
 
 export default function Video () {
     const router = useRouter()
